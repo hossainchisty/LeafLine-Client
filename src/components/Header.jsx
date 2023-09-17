f18688adaad7bdf65810d0764b1165c3b75d52a3
hossainchisty <hossain.chisty11@gmail.com>
2023-09-17 13:17:37 +0600

    style: improve header UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -78,11 +78,10 @@ const Header = ({ setSearchResults }) => {
               </div>
             </Link>
             <div className="ml-2">
-              {/* <Link to="/admin" className="text-gray-600 font-semibold">
-                Admin
-              </Link> */}
-              {/* <span className="mx-1">|</span> */}
-              <button onClick={logout} className="text-gray-600 font-semibold">
+              <button
+                onClick={logout}
+                className="text-white bg-orange-500 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 m-2 mr-2 focus:outline-none "
+              >
                 Log out
               </button>
             </div>
@@ -90,11 +89,17 @@ const Header = ({ setSearchResults }) => {
         )}
         {!isLoggedIn && (
           <div className="ml-2">
-            <Link to="/signup" className="text-gray-600 font-semibold">
+            <Link
+              to="/signup"
+              className="m-2 py-2 px-4 rounded-md duration-200 border-b border-gray-100 hover:bg-gray-100 text-black"
+            >
               Sign Up
             </Link>
-            <span className="mx-1">|</span>
-            <Link to="/signin" className="text-gray-600 font-semibold">
+
+            <Link
+              to="/signin"
+              className="m-2 py-2 px-4 rounded-md duration-200 border-b border-gray-100 hover:bg-gray-100 text-black"
+            >
               Sign In
             </Link>
           </div>
