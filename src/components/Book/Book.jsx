b3da6cf9a1f1e2c7d8f6b6384df3030de68fd7b7
hossainchisty <hossain.chisty11@gmail.com>
2023-09-20 00:26:50 +0600

    feat: Improve UI of star of reviews

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import Featured from "./Featured/Featured";
 import { FaRegHeart } from "react-icons/fa";
 import { toast } from "react-hot-toast";
+import Star from "./Star";
 
 const Book = ({ book }) => {
   const { title, author, thumbnail, price, featured, rating, _id } = book;
@@ -31,22 +32,6 @@ const Book = ({ book }) => {
     }
   };
 
-  // Define the star icons for reviews
-  const stars = Array.from({ length: rating }, (_, index) => (
-    <svg
-      key={index}
-      xmlns="http://www.w3.org/2000/svg"
-      className="w-4 h-4 text-yellow-400 fill-current"
-      viewBox="0 0 20 20"
-    >
-      <path
-        fillRule="evenodd"
-        d="M10 2a.75.75 0 01.606.306l1.894 2.56 2.928.472a.75.75 0 01.416 1.287l-2.246 2.186.53 2.92a.75.75 0 01-1.088.791L10 11.347l-2.624 1.564a.75.75 0 01-1.088-.79l.53-2.92-2.246-2.186a.75.75 0 01.416-1.287l2.928-.472 1.894-2.56a.75.75 0 01.607-.306zM10 18a.75.75 0 01.75.75v-5.632l2.295 1.372a.75.75 0 01.36 1.023l-1.297 2.705 2.423-.394a.75.75 0 01.443 1.357l-3.096 1.007-1.344 2.328a.75.75 0 01-1.33 0l-1.344-2.328-3.096-1.007a.75.75 0 01.443-1.357l2.423.394-1.297-2.705a.75.75 0 01.36-1.023L9.25 13.118V18a.75.75 0 01.75-.75z"
-        clipRule="evenodd"
-      />
-    </svg>
-  ));
-
   return (
     <div className="flex rounded-lg shadow-lg overflow-hidden relative">
       <img
@@ -66,8 +51,11 @@ const Book = ({ book }) => {
             <span className="text-blue-400">{author}</span>
           </Link>
         </p>
-
-        <div className="flex items-center space-x-2">{stars}</div>
+        <div className="flex items-center space-x-2">
+          {Array.from({ length: rating }).map((element, index) => (
+            <Star element={element} key={index} />
+          ))}
+        </div>
         <p className="text-blue-600 font-semibold text-lg">BDT {price}</p>
         <div className="flex items-center space-x-2">
           {/* Wishlist Icon/Button */}
