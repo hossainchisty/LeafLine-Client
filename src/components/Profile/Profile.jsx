1f3ff5aa1213f5ae8bcb23cbfcbd6697218bc89f
hossainchisty <hossain.chisty11@gmail.com>
2023-09-23 21:24:18 +0600

    fix: only verified user can get badge

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -72,9 +72,9 @@ function Profile() {
         />
       </div>
       <h2 className="text-3xl font-bold text-center mb-2">
-        {userData.isVerified && (
-          <div className="flex items-center justify-center">
-            <span className="mr-1 mb-3">{userData.full_name}</span>
+        <div className="flex items-center justify-center">
+          <span className="mr-1 mb-3">{userData.full_name}</span>
+          {userData.isVerified && (
             <svg
               stroke="currentColor"
               fill="currentColor"
@@ -87,8 +87,8 @@ function Profile() {
             >
               <path d="M8.38 14.72H7.67L6 13H3.53L3 12.5V10.08L1.31 8.36004V7.65004L3 5.93004V3.50004L3.53 3.00004H6L7.67 1.29004H8.38L10.1 3.00004H12.53L13.03 3.49004V5.93004L14.74 7.65004V8.36004L13 10.08V12.5L12.5 13H10.1L8.38 14.72ZM6.73004 10.4799H7.44004L11.21 6.71L10.5 6L7.09004 9.41991L5.71 8.03984L5 8.74984L6.73004 10.4799Z"></path>
             </svg>
-          </div>
-        )}
+          )}
+        </div>
       </h2>
       <Favorite />
     </div>
