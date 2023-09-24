138c7651fb2215f3388d8477bef145ba9ac0fc86
hossainchisty <hossain.chisty11@gmail.com>
2023-09-24 14:12:39 +0600

    style: fix side issues with the border

--- src/shared/Footer/Footer.jsx
+++ src/shared/Footer/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
     <footer className="bg-white">
-      <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-        <div className="md:flex md:justify-between">
+      <div className="mx-auto w-full max-w-screen-xl p-4 py-8 lg:py-4 mt-10">
+        <div className="md:flex md:justify-between ">
           <div className="mb-6 md:mb-0">
             <a
               href="https://leafline.vercel.app/"
@@ -18,27 +18,7 @@ const Footer = () => {
               </span>
             </a>
           </div>
-          <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-            <div>
-              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
-                Resources
-              </h2>
-              <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                <li className="mb-4">
-                  <a href="https://flowbite.com/" className="hover:underline">
-                    Flowbite
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://tailwindcss.com/"
-                    className="hover:underline"
-                  >
-                    Tailwind CSS
-                  </a>
-                </li>
-              </ul>
-            </div>
+          <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-2">
             <div>
               <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
                 Follow us
