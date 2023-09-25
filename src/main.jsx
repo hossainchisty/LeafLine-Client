20ca0223b60992b8d67539e8c7caae2d71a2c08b
hossainchisty <hossain.chisty11@gmail.com>
2023-09-25 23:57:03 +0600

    chore: remove raw redux

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import { Provider } from "react-redux";
-import store from "./store/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-      <Toaster position="top-right" reverseOrder={false} />
-    </BrowserRouter>
-  </Provider>
+  <BrowserRouter>
+    <App />
+    <Toaster position="top-right" reverseOrder={false} />
+  </BrowserRouter>
 );
