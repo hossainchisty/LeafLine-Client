5b6c4d9a71634e1acb9a821bfd30c404f12f225d
hossainchisty <hossain.chisty11@gmail.com>
2023-10-05 18:27:55 +0600

    feat: User profile skeleton

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,38 +1,38 @@
-import { useEffect, useState } from "react";
-import toast from "react-hot-toast";
-import LoadingIndicator from "./LoadingIndicator";
-import UserProfile from "./UserProfile";
-import { fetchUserProfile } from "../../services/userService";
+import { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
+import UserProfile from './UserProfile';
+import { fetchUserProfile } from '../../services/userService';
+import ProfileSkeleton from './ProfileSkeleton';
 
 function Profile() {
   const [userData, setUserData] = useState(null);
   const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
     async function fetchUser() {
       try {
         const user = await fetchUserProfile();
         setUserData(user);
-      } catch (error) {
-        console.error("Error fetching user data:", error);
-        toast.error("Failed to fetch user data");
-      } finally {
         setLoading(false);
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+        toast.error('Failed to fetch user data');
       }
     }
 
     fetchUser();
   }, []);
 
-  if (isLoading) {
-    return <LoadingIndicator />;
-  }
-
   if (!userData) {
     return null;
   }
 
-  return <UserProfile user={userData} />;
+  return (
+    <div>
+      {isLoading ? <ProfileSkeleton /> : <UserProfile user={userData} />}
+    </div>
+  );
 }
 
 export default Profile;
