c63e150706a2268be449395c612f070965baec2d
hossainchisty <hossain.chisty11@gmail.com>
2023-10-06 22:08:43 +0600

    feat: Improve book lists showing by filters, sorts and  some method of an array

--- src/components/Book/BookList.jsx
+++ src/components/Book/BookList.jsx
@@ -4,6 +4,7 @@ import Book from './Book';
 const BookList = ({ books, isLoading }) => {
   return (
     <div className='container mx-auto'>
+      {/* Book Lists */}
       <h2 className='mt-5 mb-5 text-xl font-bold'>
         {isLoading ? '' : `Book List`}
       </h2>
@@ -13,24 +14,45 @@ const BookList = ({ books, isLoading }) => {
         ))}
       </div>
 
-      <h2 className='mt-5 mb-5 text-xl font-bold'>
-        {isLoading ? '' : `Popular Products`}
-      </h2>
+      {/* Most Popular Books */}
+      {books.some((book) => book.averageRating > 1000) && (
+        <div>
+          <h2 className='mt-5 mb-5 text-xl font-bold'>
+            {isLoading ? '' : `Popular Books`}
+          </h2>
+          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4'>
+            {books
+              .filter((book) => book.averageRating > 1000) // Filter books with averageRating > 1000
+              .map((book, index) => (
+                <Book key={index} book={book} isLoading={isLoading} />
+              ))}
+          </div>
+        </div>
+      )}
 
+      {/* Featured Books */}
+      <h2 className='mt-5 mb-5 text-xl font-bold'>
+        {isLoading ? '' : `Featured Books`}
+      </h2>
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4'>
-        {books.map((book, index) => (
-          <Book key={index} book={book} isLoading={isLoading} />
-        ))}
+        {books
+          .filter((book) => book.featured) // Filter only featured books
+          .map((book, index) => (
+            <Book key={index} book={book} isLoading={isLoading} />
+          ))}
       </div>
 
+      {/* New Arrival Books */}
       <h2 className='mt-5 mb-5 text-xl font-bold'>
         {isLoading ? '' : `New Arrival Books`}
       </h2>
 
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4'>
-        {books.map((book, index) => (
-          <Book key={index} book={book} isLoading={isLoading} />
-        ))}
+        {books
+          .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)) // Sort books by creation timestamp in descending order
+          .map((book, index) => (
+            <Book key={index} book={book} isLoading={isLoading} />
+          ))}
       </div>
 
       {/* Show the button only when isLoading is false */}
